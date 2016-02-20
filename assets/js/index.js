a2311157326cc73b6bae6afe2a761afa1a52622c
BestMatEver <mattkun@gmail.com>
2016-02-20 18:08:53 -0500

    randomized icons and card matching are in. time to takle hearts and win/lose screens.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ var iconChoices = ["fa-glass", "fa-music", "fa-arrow-circle-o-down", "fa-search"
 
 var numBoxes = 0;//the number of boxes the user enters
 var iconsThisGame=[];//an array of the icons in use THIS game.
+var iconsThisGame2=[];
 var clickedNum =0; //this is how many times we've clicked a card
 var card1; //the first card we want to compare.
 
@@ -18,71 +19,93 @@ numBoxes = parseInt($("#numBoxes").val());
   numBoxes+=1;
 }//end if
 
-for(var k=-2; k <= (numBoxes); k=k+1)//this for loop selects icons to be used THIS GAME.
+var idx1=0;
+
+for(var k=0; k <= ((numBoxes/2)-1); k++)//this for loop selects a number of icons to be used THIS GAME that is half as many boxes as the user entered.
 {
-  var icon = iconChoices[Math.floor(Math.random() * (200- 0 + 1)) + 0];//gets a random icon from the big icon array.
+  idx1 = Math.floor(Math.random() * (iconChoices.length- 0 + 1)) + 0//index of random icon
+
+  if(idx1!=0)//the reason for this if statment is described in a similar structure below.
+  {
+    idx1 = idx1-1;
+  }
+
+  var icon = iconChoices[idx1];//gets a random icon from the big icon array.
   if(iconsThisGame.indexOf(icon) === -1)//this if makes sure the same icon isnt put into the array twice.
   {
-    iconsThisGame[k] = icon;
-    iconsThisGame[k+1] = icon;//we put two of the same icon in because when we put them in the game, we're going to to splice them out. so we need as many as we're going to need for the game.
+    //we need two of each icon, so we add it to two different arrays.
+    iconsThisGame[k] = icon;// we use two arrays to keep our for loop more predictible.
+    iconsThisGame2[k] = icon;//we're going to combine them into one array when we're done
   }
   else
   {
-    console.log("fuck you.");
-    k=k-2;
-  }//it tried to grab the same icon twice.
+    console.log("fuck you.");//it tried to grab the same icon twice.
+    k--;
+  }
 }//end for loop
 
-if(iconsThisGame.length > numBoxes)
-{
-  console.log("IM BIGGER THAN I SHOULD BE!");
-  iconsThisGame.splice(numBoxes, (iconsThisGame.length - numBoxes));
-}
+iconsThisGame = $.merge(iconsThisGame, iconsThisGame2);//this combines the two arrays into one. this array should now hold two of every icon used in this game.
 
-
-for(var p=0; p < iconsThisGame.length; p++)//this for loop is just for debugging purposes.
-{
-  console.log(iconsThisGame[p]);
-}
-console.log(iconsThisGame);//debug. prints out the whole array of icons used this game.
 console.log("icons this game: " + iconsThisGame.length);
+console.log(iconsThisGame);
 
-for(var i = 0;i<=numBoxes-1;i++)//make the board.
+
+
+for(var i = 1;i<=numBoxes;i++)//make the board.
 {
   var idx = Math.floor(Math.random() * (iconsThisGame.length - 0 + 1)) + 0 //this generates a random number that is between 0 and the length of the iconsThisGame array.
+  if (idx!=0)
+  {
+    idx =idx-1;//lets say the iconsthisgame has a length of 3, since array indicies start at 0, that means the index for the last entry in the array is 2 (0, 1, 2 = 3 elements). so we need to subtract one to make sure it dosent have an array out of bounds error. BUT ONLY IF it isnt already 0. if it's zero and we subtract one, we'll get -1 which will give us an array out of bounds error but in the oppsite direction. sheesh. this feels hacky. but whatever. it works.
+  }//now that i think about it, the formula for random numbers i found was an INCLUSIVE one, meaning it can select the max number. if i had just taken an EXCLUSIVE formula (or even just subtracted 1 from the max in the formula) i could have avoided having weird if statments hanging around. dont care though. it works now. just need to remember for next time.
+
   var curcon = iconsThisGame[idx];
-  console.log("iconsThisGame was: " + iconsThisGame.length);
+  console.log("--------CARD " + i +" Start--------- Index of: " + idx);//debug
+  console.log("grabbing: " + iconsThisGame[idx])//debug
   iconsThisGame.splice(idx, 1);//removes the icon that's going into a card from the iconsThisGame, so it cant be picked again.
-  console.log("and became: " + iconsThisGame.length);
+  console.log("iconsThisGame = " + iconsThisGame.length);//debug
+  console.log(iconsThisGame);//debug
 
 
 //the following code injects HTML into the page for each card the player wanted.
   $("#gameZone").append("<span class='flipper'><span class='card'><i class='fa " + curcon + " fa-4x'></i></span></span>");
   $("#question").css("display", "none");
-}
+}//end the for loop
 
 });//end generate click
 
 
   $("#gameZone").on("click", ".card", function(){
+    clickedNum++;
 
-     $(this).toggleClass("cardOpen"); //this is wrong but good for debugging.
+    if(clickedNum%2!=0)
+    {//it saves the card clicked on every odd click. ie: 1, 3, 5 etc.
+      card1 = $(this);
+      console.log(card1.html());
+    }
+    else//on every even click we check to see if the cards are the same.
+    {
+      if(card1.html() === $(this).html())
+        {
+          card1.addClass("permOpen");
+          $(this).addClass("permOpen");
+        }
+    }
 
+    //$(this).toggleClass("cardOpen"); //this is wrong but good for debugging.
 //what's below works as the game ought to work, but its really annoying for debugging purposes.
-    // clickedNum++
-    // if(clickedNum >= 3)
-    // {
-    //   if(((clickedNum-3)%2===0))
-    //   {
-    //     $(".card").removeClass("cardOpen");
-    //   }
-    //   else if(clickedNum===3)
-    //   {
-    //     $(".card").removeClass("cardOpen");
-    //   }
-    // }
-    // $(this).addClass("cardOpen");
-    // console.log($(this));
+    if(clickedNum >= 3)
+    {
+      if(((clickedNum-3)%2===0))
+      {
+        $(".card").removeClass("cardOpen");
+      }
+      else if(clickedNum===3)
+      {
+        $(".card").removeClass("cardOpen");
+      }
+    }
+    $(this).addClass("cardOpen");
 
 
   });//end card click
